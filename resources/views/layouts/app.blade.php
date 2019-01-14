0fd802fc1d8f9a48539be1d1a82bafbfe87e5ef5
Alexius <alexiuslim1994@gmail.com>
2019-01-14 21:48:32 +0800

    14/01/2019 - Revert back to using assets for loading resource.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Global Family Survey') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ env('DOMAIN') }}js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
