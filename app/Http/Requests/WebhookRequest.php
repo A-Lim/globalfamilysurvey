d62aa75340c62b00b901aa9a5414a2d32fd91f22
Alexius <alexiuslim1994@gmail.com>
2019-02-11 12:20:15 +0800

    Update delete webhook

--- app/Http/Requests/WebhookRequest.php
+++ app/Http/Requests/WebhookRequest.php
@@ -88,4 +88,71 @@ class WebhookRequest extends FormRequest
             return ['status' => false, 'message' => 'Status Code ('.$error->http_status_code.'): '.$error->message, 'content' => $contents];
         }
     }
+
+    protected function update_webhook_request($token) {
+        $webhook = Webhook::find($this->webhook->id);
+        $client = new Client();
+        try {
+            $response = $client->patch(Webhook::URL_WEBHOOK, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer '.$token
+                ],
+                'json' => [
+                    'name' => request('name'),
+                    'event_type' => request('event_type'),
+                    'object_type' => request('object_type'),
+                    'object_ids' => [request('survey_id')],
+                    'subscription_url' => url('api/surveys/'.request('survey_id').'/subscription'),
+                ]
+            ]);
+
+            $contents = $response->getBody()->getContents();
+            $json = json_decode($contents);
+
+            // if there is no error
+            if (!isset($json->error)) {
+                $webhook->update([
+                    'name' => request('name'),
+                    'type' => request('type'),
+                    'survey_id' => request('survey_id'),
+                    'subscription_url' => url('api/surveys/'.request('survey_id').'/subscription'),
+                ]);
+            }
+
+            return ['status' => true, 'content' => $response->getBody()->getContents()];
+        } catch (\Exception $exception) {
+            $error = json_decode($exception->getResponse()->getBody()->getContents())->error;
+            $contents = json_decode($exception->getResponse()->getBody());
+            return ['status' => false, 'message' => 'Status Code ('.$error->http_status_code.'): '.$error->message, 'content' => $contents];
+        }
+    }
+
+
+    protected function delete_webhook_request($token) {
+        $webhook = Webhook::find($this->webhook->id);
+        $client = new Client();
+        try {
+            $response = $client->delete(Webhook::URL_WEBHOOK.'/'.$webhook->id, [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer '.$token
+                ]
+            ]);
+
+            $contents = $response->getBody()->getContents();
+            $json = json_decode($contents);
+
+            // if there is no error
+            if (!isset($json->error))
+                $webhook->delete();
+
+            return ['status' => true, 'content' => $response->getBody()->getContents()];
+        } catch (\Exception $exception) {
+            $error = json_decode($exception->getResponse()->getBody()->getContents())->error;
+            $contents = json_decode($exception->getResponse()->getBody());
+            return ['status' => false, 'message' => 'Status Code ('.$error->http_status_code.'): '.$error->message, 'content' => $contents];
+        }
+    }
+
 }
